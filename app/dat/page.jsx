2ce37b69b8defc948d252f032faac4ad183da492
Shawky Elshazly <shawkyelshazly2@gmail.com>
2024-01-21 05:07:56 +0200

    starting dat

---
+++ app/dat/page.jsx
@@ -0,0 +1,14 @@
+export default async function Page() {
+	return (
+		<div className="w-full flex container gap-4">
+			<div className="flex flex-col h-full w-full py-4 gap-4 container items-center">
+				<div className="flex flex-row w-full container justify-between gap-4 bg-white mt-[-16px]  py-4 px-6">
+					<h1 className="text-2xl text-gray-400 font-semibold italic">
+						DISCIPLINARY ACTIONS
+					</h1>
+					<div className="flex flex-row gap-1">actions</div>
+				</div>
+			</div>
+		</div>
+	);
+}
